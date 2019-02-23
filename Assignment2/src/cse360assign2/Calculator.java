131386fc015cc7be3f6afd92d7a8d4b8d8e1ad71
shivam2345 <32351363+shivam2345@users.noreply.github.com>
2019-02-22 21:59:40 -0700

    Create the third version

--- Assignment2/src/cse360assign2/Calculator.java
+++ Assignment2/src/cse360assign2/Calculator.java
@@ -1,28 +1,36 @@
 
 package cse360assign2;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Calculator {
 
 	private int total;
+	private String history;
 	
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
+		history = "" + total;
 	}
 	
 	public int getTotal () {
 		return 0;
-	}
+	}                                                                                                                                                                  
 	
 	public void add (int value) {
 		total += value;
+		history = history + " + " + value;
 	}
 	
 	public void subtract (int value) {
 		total -= value;
+		history = history + " - " + value;
 	}
 	
 	public void multiply (int value) {
 		total *= value;
+		history = history + " * " + total;
 	}
 	
 	public void divide (int value) {
@@ -32,9 +40,10 @@ public class Calculator {
 		else {
 			total /= value;
 		}
+		history = history + " / " + value;
 	}
 	
 	public String getHistory () {
-		return "";
+		return history;
 	}
 }
